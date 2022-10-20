c8d790305d35a42deecf58bb146c0df5f006b60c
sakho13 <wecl.khotaro@gmail.com>
2022-10-20 11:39:22 +0900

    公開条件設定ミス修正

--- utils/notion_api.ts
+++ utils/notion_api.ts
@@ -1,16 +1,19 @@
 import { Client } from "@notionhq/client"
-import { PageObjectResponse, PartialPageObjectResponse } from "@notionhq/client/build/src/api-endpoints";
+import {
+  PageObjectResponse,
+  PartialPageObjectResponse,
+} from "@notionhq/client/build/src/api-endpoints"
 
 export class NotionApi {
-  private notion: Client;
-  private dbId: string;
+  private notion: Client
+  private dbId: string
 
   constructor(notionKey: string, notionDbId: string) {
     // console.log("nApi: ", notionKey, notionDbId)
     this.notion = new Client({
       auth: notionKey,
-    });
-    this.dbId = notionDbId;
+    })
+    this.dbId = notionDbId
   }
 
   /**
@@ -23,8 +26,14 @@ export class NotionApi {
       database_id: this.dbId,
       filter: {
         and: [
+          {
+            property: "Published",
+            checkbox: {
+              equals: true,
+            },
+          },
           // {
-          //   property: "Published",
+          //   property: "Test",
           //   checkbox: {
           //     equals: true,
           //   },
@@ -33,12 +42,6 @@ export class NotionApi {
             property: "Slug",
             rich_text: {
               is_not_empty: true,
-            },
-          },
-          {
-            property: "Test",
-            checkbox: {
-              equals: true,
             },
           },
         ],
@@ -52,7 +55,7 @@ export class NotionApi {
       start_cursor: nextCursor === null ? undefined : nextCursor,
       page_size: 2,
     })
-    return res;
+    return res
   }
 
   /**
@@ -61,15 +64,21 @@ export class NotionApi {
    * @returns
    */
   public async getPost(
-    slug: string
+    slug: string,
   ): Promise<[PageObjectResponse | PartialPageObjectResponse | null, any[]]> {
-    console.log(slug);
+    console.log(slug)
     const res1 = await this.notion.databases.query({
       database_id: this.dbId,
       filter: {
         and: [
+          {
+            property: "Published",
+            checkbox: {
+              equals: true,
+            },
+          },
           // {
-          //   property: "Published",
+          //   property: "Test",
           //   checkbox: {
           //     equals: true,
           //   },
@@ -80,27 +89,20 @@ export class NotionApi {
               equals: slug,
             },
           },
-          {
-            property: "Test",
-            checkbox: {
-              equals: true,
-            },
-          },
         ],
       },
       page_size: 1,
     })
 
-
-    if (res1.results.length === 1 && 'url' in res1.results[0]) {
+    if (res1.results.length === 1 && "url" in res1.results[0]) {
       const page_id = res1.results[0].id
       const res2 = await this.notion.blocks.children.list({
         block_id: page_id,
       })
-      return [res1.results[0], res2.results];
+      return [res1.results[0], res2.results]
     }
 
-    return [null, []];
+    return [null, []]
   }
 
   public async getBlockChild(blockId: string) {
